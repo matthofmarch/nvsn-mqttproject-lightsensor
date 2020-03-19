b65647c36cda916b54db03db89f189850a7faf33
Kevin Mohammed <kevinkamilmohammed16@gmail.com>
2020-03-19 13:15:01 +0100

    added led to mqtt client

--- mqttclient.py
+++ mqttclient.py
@@ -1,5 +1,9 @@
 import paho.mqtt.client as mqtt #import the client1
 import time
+import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and set initial value to low (off)
 broker_address="52.157.91.193" 
 
 def on_message(client, userdata, message):
@@ -8,6 +12,11 @@ def on_message(client, userdata, message):
     print("message qos=",message.qos)
     print("message retain flag=",message.retain)
 
+def handle_message(message):
+    if on:
+         GPIO.output(8, GPIO.HIGH) # Turn off
+    else 
+         GPIO.output(8, GPIO.LOW) # Turn off
 
 client = mqtt.Client(client_id="P1") #create new instance
 client.connect(broker_address) #connect to broker
