fc955ba8d036e4a21239de7fba4be77b172dbe7d
Kevin Mohammed <kevinkamilmohammed16@gmail.com>
2020-03-24 13:14:25 +0100

    add led

--- blink.py
+++ blink.py
@@ -5,9 +5,9 @@ GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(14, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and set initial value to low (off)
 GPIO.setup(15, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and set initial value to low (off)
 while True: # Run forever
- GPIO.output(14, GPIO.HIGH) # Turn on
- GPIO.output(15, GPIO.HIGH) # Turn on
+ GPIO.output(8, GPIO.HIGH) # Turn on
+ GPIO.output(10, GPIO.HIGH) # Turn on
  sleep(1) # Sleep for 1 second
- GPIO.output(14, GPIO.LOW) # Turn off
- GPIO.output(15, GPIO.LOW) # Turn off
+ GPIO.output(8, GPIO.LOW) # Turn off
+ GPIO.output(10, GPIO.LOW) # Turn off
  sleep(1) # Sleep for 1 second
