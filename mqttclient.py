14c2584935c3f5edaa182384a9d02a61d0774303
Kevin Mohammed <kevinkamilmohammed16@gmail.com>
2020-04-16 09:58:57 +0200

    led

--- mqttclient.py
+++ mqttclient.py
@@ -6,6 +6,7 @@ import json
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and set initial value to low (off)
+GPIO.setup(10, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and set initial value to low (off)
 broker_address="51.136.13.51" 
 
 def on_message(client, userdata, message):
